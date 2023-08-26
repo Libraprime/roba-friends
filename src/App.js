abdb93144003ba318b0f13763ec194bef0d30671
Chisom <uchisom10@gmail.com>
2023-08-26 02:40:47 -0700

    made my initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Card from './Components/Card';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <section>
+        <Card />
+        <Card />
+        <Card />
+      </section>
+    </>
+    
+  )
+}
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,10 @@
+const Card = () => {
+    return (
+        <a href="">
+            <img src="" alt="" />
+            <p>Cards</p>
+        </a>
+    )
+}
+
+export default Card
