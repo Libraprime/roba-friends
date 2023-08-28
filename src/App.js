e31e68caa77096be7030f13b0ee537ac19777a42
Chisom <uchisom10@gmail.com>
2023-08-28 02:46:27 -0700

    made changes to the card list location and created a new cardlist component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './Components/Header';
 import CardList from './Components/CardList';
+import Searchbox from './Components/Searchbox';
 
 
 const App = () => {
   return (
     <>
       <Header />
+      <Searchbox />
       <CardList />
     </>
     
---
+++ src/Components/Searchbox.js
@@ -0,0 +1,13 @@
+const Searchbox = () => {
+    return(
+        <div className="pa2"> 
+            <input 
+                className="pa3 ba b--green bg-lightest-blue"
+                type="search" 
+                placeholder="Find robots" 
+            />
+        </div>
+    );
+}
+
+export default Searchbox
